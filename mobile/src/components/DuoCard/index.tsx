ffe3096ca932e69fa4bd2060be7dc2a63f53a6ca
Jamzmarks <jamzmarks@gmail.com>
2022-10-06 10:06:35 -0300

    att

---
+++ mobile/src/components/DuoCard/index.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { View, TouchableOpacity, Text} from 'react-native';
+import {GameController} from 'phosphor-react-native'
+import { styles } from './styles';
+import { DuoInfo } from '../DuoInfo';
+import { THEME } from '../../theme';
+
+export interface DuoCardProps{
+    hourEnd: string,
+    hourStart: string,
+    id: string,
+    name: string,
+    useVoiceChannel: boolean,
+    weekDays: string[]
+    yearPlaying: number,
+}
+
+interface Props {
+    data: DuoCardProps;
+}
+
+export function DuoCard({data }: Props) {
+  return (
+    <View style={styles.container}>
+        <DuoInfo
+            label='Nome'
+            value={data.name}
+        />
+        <DuoInfo
+            label='Tempo de jogo'
+            value={`${data.yearPlaying} anos`}
+        />
+        <DuoInfo
+            label='Disponibilidade'
+            value={`${data.weekDays.length} dias \u2022 ${data.hourStart} - ${data.hourEnd}`}
+        />
+        <DuoInfo
+            label='Chamada de áudio'
+            value={data.useVoiceChannel ? "Sim" : "Não"}
+            colorValue={data.useVoiceChannel ? THEME.COLORS.SUCCESS : THEME.COLORS.ALERT}
+        />
+        <TouchableOpacity
+            style={styles.button}>
+                <GameController
+                    color={THEME.COLORS.TEXT}
+                    size={20}
+                />
+                <Text style={styles.buttonTitle}>
+                    Conectar
+                </Text>
+        </TouchableOpacity>
+    </View>
+  );
+}
--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,38 +1,54 @@
+import {useEffect, useState} from 'react';
 import { View, Image, FlatList } from 'react-native';
-
-import LogoImg from '../../assets/logo-nlw-esports.png'
-import { Heading } from '../../components/Heading';
-
-import { GameCard } from '../../components/GameCard';
-import { GAMES } from '../../utils/games'
-
+import {SafeAreaView} from 'react-native-safe-area-context'
+import {useNavigation} from '@react-navigation/native'
 import { styles } from './styles';
+import LogoImg from '../../assets/logo-nlw-esports.png'
+
+import { Heading } from '../../components/Heading';
+import { GameCard, GameCardProps } from '../../components/GameCard';
+import { Background } from '../../components/background';
+
 
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([])
+
+  const navigation = useNavigation()
+  function handleOpenGame({id, title, bannerUrl}: GameCardProps){
+    navigation.navigate('game', {id, title, bannerUrl})
+  }
+  
+  useEffect(() => {
+    fetch('http://192.168.15.61:3333/games')
+    .then(response => response.json())
+    .then(data => setGames(data))
+  }, [])
+
   return (
-    <View style={styles.container}>
-        <Image
-            source={LogoImg}
-            style={styles.Logo}
-        />
-        <Heading
-        title="Encontre seu duo!"
-        subtitle="Selecione o game que deseja jogar..."/>
-        <FlatList
-          data={GAMES}
-          keyExtractor={item => item.id}
-          renderItem={({item})=> (
-            <GameCard
-              data={item}
-            />
-          )}
-          showsHorizontalScrollIndicator={false}
-          horizontal
+    <Background>
+      <SafeAreaView style={styles.container}>
+          <Image
+              source={LogoImg}
+              style={styles.Logo}
+          />
+          <Heading
+          title="Encontre seu duo!"
+          subtitle="Selecione o game que deseja jogar..."/>
+          <FlatList
+            data={games}
+            keyExtractor={item => item.id}
+            renderItem={({item})=> (
+              <GameCard
+                data={item}
+                onPress={() => handleOpenGame(item)}
+              />
+            )}
+            showsHorizontalScrollIndicator={false}
+            horizontal
 
-          contentContainerStyle={styles.contentList}
-        />
-
-        
-    </View>
+            contentContainerStyle={styles.contentList}
+          />   
+      </SafeAreaView>
+    </Background>
   );
 }
