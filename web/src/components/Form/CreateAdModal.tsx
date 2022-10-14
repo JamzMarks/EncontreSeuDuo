3cfac661291491e12e21e2b8e9c88d9a2c15e65e
Jamzmarks <jamzmarks@gmail.com>
2022-10-14 02:07:35 -0300

    a

--- web/src/components/Form/CreateAdModal.tsx
+++ web/src/components/Form/CreateAdModal.tsx
@@ -3,8 +3,26 @@ import * as Dialog from '@radix-ui/react-dialog'
 import {Input} from './Input'
 import * as Checkbox from '@radix-ui/react-checkbox'
 import {Check} from 'phosphor-react'
+import { useEffect, useState } from 'react'
+import * as ToggleGroup from '@radix-ui/react-toggle-group'
+
+interface Game {
+  id: string;
+  title: string;
+}
 
 export function CreateAdModal() {
+
+  const [games, setGames] = useState<Game[]>([])
+
+  useEffect(() => {
+    fetch('http://localhost:3333/games')
+    .then(response => response.json())
+    .then(data => {
+      setGames(data)
+    })
+  }, [])
+
     return(
         <Dialog.Portal>
             <Dialog.Overlay className='bg-black/60 inset-0 fixed'>
@@ -20,9 +38,14 @@ export function CreateAdModal() {
                             <label className='font-semibold' htmlFor="game">Qual o game?</label>
                             <select 
                                 id="game" 
-                                className='bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500'
+                                className='bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500 appearance-none'
                             >
                                 <option selected disabled value="">Selecione o game que deseja jogar</option>
+                               
+                                {games.map(game => {
+                                  return <option key={game.id} value={game.id}>{game.title}</option>
+  
+                                })}
                             </select>
                         </div>
 
@@ -46,29 +69,37 @@ export function CreateAdModal() {
                           <div className='flex flex-col gap-2'>
                             <label htmlFor="weekDays">Quando costuma jogar?</label>
 
-                            <div className='grid grid-cols-4 gap-2 '>
-                              <button
-                               className='w-8 h-8 rounded bg-zinc-900'
-                               title='Domingo'>D</button>
-                              <button
-                               className='w-8 h-8 rounded bg-zinc-900'
-                               title='Segunda'>S</button>
-                              <button
-                               className='w-8 h-8 rounded bg-zinc-900'
-                               title='Terça'>T</button>
-                              <button
-                               className='w-8 h-8 rounded bg-zinc-900'
-                               title='Quarta'>Q</button>
-                              <button
-                               className='w-8 h-8 rounded bg-zinc-900'
-                               title='Quinta'>Q</button>
-                              <button
-                               className='w-8 h-8 rounded bg-zinc-900'
-                               title='Sexta'>S</button>
-                              <button
-                               className='w-8 h-8 rounded bg-zinc-900'
-                               title='Sábado'>S</button>
-                            </div>
+
+                              <ToggleGroup.Root type='multiple' className='grid grid-cols-4 gap-2'>
+                                <ToggleGroup.Item
+                                value="0"
+                                className='w-8 h-8 rounded bg-zinc-900'
+                                title='Domingo'>D</ToggleGroup.Item>
+                                <ToggleGroup.Item
+                                value="1"
+                                className='w-8 h-8 rounded bg-zinc-900'
+                                title='Segunda'>S</ToggleGroup.Item>
+                                <ToggleGroup.Item
+                                value="2"
+                                className='w-8 h-8 rounded bg-zinc-900'
+                                title='Terça'>T</ToggleGroup.Item>
+                                <ToggleGroup.Item
+                                value="3"
+                                className='w-8 h-8 rounded bg-zinc-900'
+                                title='Quarta'>Q</ToggleGroup.Item>
+                                <ToggleGroup.Item
+                                value="4"
+                                className='w-8 h-8 rounded bg-zinc-900'
+                                title='Quinta'>Q</ToggleGroup.Item>
+                                <ToggleGroup.Item
+                                value="5"
+                                className='w-8 h-8 rounded bg-zinc-900'
+                                title='Sexta'>S</ToggleGroup.Item>
+                                <ToggleGroup.Item
+                                value="6"
+                                className='w-8 h-8 rounded bg-zinc-900'
+                                title='Sábado'>S</ToggleGroup.Item>
+                              </ToggleGroup.Root>
 
                           </div>
                           <div className='flex flex-col gap-2 flex-1'>
